25ca7213b819443714494ab8362e2e9c899c4313
hua-bang <47221942+hua-bang@users.noreply.github.com>
2022-03-07 00:48:09 +0800

    feat: add Authorized component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Authorized from './components/Auth/Authorized'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,34 +10,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <Authorized authority={'admin'} noMatch={null} userInfo={{ currentAuthority: 'admin' }}>
+          <button>进入管理后台</button>
+        </Authorized>
       </header>
     </div>
   )
